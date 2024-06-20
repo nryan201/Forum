5e59cf2c30e3264f2944d94be7fe9f0953b1cb8c
louis <louis.margras@ynov.com>
2024-06-20 20:05:10 +0200

    [Post] Login/Register/Password page commit2

--- template/script/connexion.js
+++ template/script/connexion.js
@@ -1,42 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
     var loginForm = document.getElementById('loginForm');
     var registerForm = document.getElementById('registerForm');
+    var forgotPassword = document.getElementById('forgotPassword');
 
+    // Fonction pour afficher le formulaire d'enregistrement
     function showRegisterForm(e) {
         e.preventDefault();
         loginForm.style.display = 'none';
+        forgotPassword.style.display = 'none';
         registerForm.style.display = 'block';
-        attachLoginLinkListener();
     }
 
+    // Fonction pour afficher le formulaire de connexion
     function showLoginForm(e) {
         e.preventDefault();
-        loginForm.style.display = 'block';
         registerForm.style.display = 'none';
-        attachRegisterLinkListener();
+        forgotPassword.style.display = 'none';
+        loginForm.style.display = 'block';
     }
 
-    function attachRegisterLinkListener() {
-        var registerLink = document.querySelector('.register-link a[href="#registerForm"]');
+    // Fonction pour afficher le formulaire de mot de passe oublié
+    function showForgotPasswordForm(e) {
+        e.preventDefault();
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'none';
+        forgotPassword.style.display = 'block';
+    }
+
+    // Attacher les écouteurs d'événements
+    function attachEventListeners() {
+        var registerLink = document.querySelector('a[href="#registerForm"]');
+        var loginLink = document.querySelector('a[href="#loginForm"]');
+        var forgotPasswordLink = document.querySelector('a[href="#ForgotPassword"]');
+
         if (registerLink) {
             registerLink.addEventListener('click', showRegisterForm);
-            console.log('Register link listener attached');
-        } else {
-            console.log('Register link not found.');
         }
-    }
-
-    function attachLoginLinkListener() {
-        var loginLink = document.querySelector('.login-link a[href="#loginForm"]');
         if (loginLink) {
             loginLink.addEventListener('click', showLoginForm);
-            console.log('Login link listener attached');
-        } else {
-            console.log('Login link not found.');
+        }
+        if (forgotPasswordLink) {
+            forgotPasswordLink.addEventListener('click', showForgotPasswordForm);
+        }
+        if (loginLink) {
+            loginLink.addEventListener('click', showLoginForm);
         }
     }
 
-    // Attach initial listeners
-    attachRegisterLinkListener();
-    attachLoginLinkListener();
+    // Initialiser les écouteurs d'événements
+    attachEventListeners();
 });
