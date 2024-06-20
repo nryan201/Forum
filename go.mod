0d8a72a5a6245d564a46f604857cda22db307360
AXSS <alexis.redaud@ynov.com>
2024-06-20 14:48:20 +0200

    [Database]  "Add the SQL database to the     project.And also add the .env file to the     project."

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module main
 
 go 1.21.0
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-	"main/back"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
 )
 
-func main () {
-	back.Server()
+func main() {
+	// Charger les variables d'environnement
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Vérifier que les variables d'environnement sont chargées
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		log.Fatalf("Missing required environment variables")
+	}
+
+	// Construire la chaîne de connexion
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName)
+
+	// Connexion à la base de données
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Vérifier la connexion
+	var greeting string
+	err = conn.QueryRow(context.Background(), "SELECT 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		log.Fatalf("QueryRow failed: %v\n", err)
+	}
+
+	fmt.Println(greeting)
 }
