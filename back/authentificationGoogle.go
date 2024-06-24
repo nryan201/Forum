e528ee097c87e9c7022bf3abb7ec23662fc41354
AXSS <alexis.redaud@ynov.com>
2024-06-24 13:57:12 +0200

    [authentificationGoogle.go] link with the     database (theorycaly)

---
+++ back/authentificationGoogle.go
@@ -0,0 +1,102 @@
+package back
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     "767814867927-m0ou233go88bec08h0qobi3v50nn5qhg.apps.googleusercontent.com",
+		ClientSecret: "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",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+		Endpoint:     google.Endpoint,
+	}
+	oauthStateString = "randomstring"
+)
+
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != oauthStateString {
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	token, err := googleOauthConfig.Exchange(context.Background(), r.FormValue("code"))
+	if err != nil {
+		log.Printf("Could not get token: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		log.Printf("Could not create request: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	defer response.Body.Close()
+
+	var googleUser struct {
+		ID      string `json:"id"`
+		Email   string `json:"email"`
+		Name    string `json:"name"`
+		Picture string `json:"picture"`
+	}
+
+	if err := json.NewDecoder(response.Body).Decode(&googleUser); err != nil {
+		log.Printf("Could not decode response: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "./mydatabase.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var userID int
+	err = db.QueryRow("SELECT id FROM users WHERE google_id = ? OR email = ?", googleUser.ID, googleUser.Email).Scan(&userID)
+	if err == sql.ErrNoRows {
+		// User does not exist, create a new user
+		statement, err := db.Prepare("INSERT INTO users (username, email, google_id) VALUES (?, ?, ?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer statement.Close()
+		res, err := statement.Exec(googleUser.Name, googleUser.Email, googleUser.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		userID64, err := res.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+		userID = int(userID64)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create session for user, etc.
+	fmt.Fprintf(w, "User ID: %d\n", userID)
+	fmt.Fprintf(w, "User Info: %s\n", googleUser.Name)
+}
+
+func main() {
+	http.HandleFunc("/login", handleGoogleLogin)
+	http.HandleFunc("/callback", handleGoogleCallback)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.0
 
 require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22
 	golang.org/x/oauth2 v0.21.0 // indirect
 )
