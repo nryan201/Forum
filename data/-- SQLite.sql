9824ae8aa2e0abd5f582b7c02036687a727644e1
AXSS <alexis.redaud@ynov.com>
2024-06-26 14:07:11 +0200

    [DataAuthentification] update SQLITE.sql file

--- data/-- SQLite.sql
+++ data/-- SQLite.sql
@@ -1,18 +1,26 @@
 -- SQLite
 
+-- Table Users
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL, 
+    email TEXT UNIQUE,
+    google_id TEXT UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table Topics
 CREATE TABLE IF NOT EXISTS topics (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT, 
-    email TEXT UNIQUE,
-    google_id TEXT UNIQUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
+-- Table Comments
 CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     topic_id INTEGER NOT NULL,
@@ -23,39 +31,37 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL, 
-    email TEXT UNIQUE,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
+-- Table Hashtags
 CREATE TABLE IF NOT EXISTS hashtags (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
 );
 
+-- Table Topic_Hashtags (many-to-many relationship between topics and hashtags)
 CREATE TABLE IF NOT EXISTS topic_hashtags (
     topic_id INTEGER NOT NULL,
     hashtag_id INTEGER NOT NULL,
+    PRIMARY KEY (topic_id, hashtag_id),
     FOREIGN KEY (topic_id) REFERENCES topics (id) ON DELETE CASCADE,
-    FOREIGN KEY (hashtag_id) REFERENCES hashtags (id) ON DELETE CASCADE,
-    PRIMARY KEY (topic_id, hashtag_id)
+    FOREIGN KEY (hashtag_id) REFERENCES hashtags (id) ON DELETE CASCADE
 );
 
+-- Table Categories
 CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
 );
 
+-- Table Topic_Categories (many-to-many relationship between topics and categories)
 CREATE TABLE IF NOT EXISTS topic_categories (
     topic_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
+    PRIMARY KEY (topic_id, category_id),
     FOREIGN KEY (topic_id) REFERENCES topics (id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE,
-    PRIMARY KEY (topic_id, category_id)
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
+
+-- Table Likes
 CREATE TABLE IF NOT EXISTS likes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     topic_id INTEGER NOT NULL,
