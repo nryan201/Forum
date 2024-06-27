8d1e74121a49aa9453f24f2cadbe2a3c631a2cf7
carkevin <kevin.carles@ynov.com>
2024-06-27 14:05:34 +0200

    easter egg

--- template/script/hashtag.js
+++ template/script/hashtag.js
@@ -1,26 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('createHashtagForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+    const form = document.getElementById('createHashtagForm');
+    const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+    let currentInput = [];
 
-        let hashtagName = document.querySelector('.createHashtags').value.trim();
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            let hashtagName = document.querySelector('.createHashtags').value.trim();
+            if (hashtagName !== '') {
+                if (!hashtagName.startsWith('#')) {
+                    hashtagName = '#' + hashtagName;
+                }
 
-        if (hashtagName !== '') {
-            if (!hashtagName.startsWith('#')) {
-                hashtagName = '#' + hashtagName;
+                const newHashtag = document.createElement('li');
+                newHashtag.textContent = hashtagName;
+                document.getElementById('hashtagsList').appendChild(newHashtag);
+                document.querySelector('.createHashtags').value = '';
+
+                // Send form data using AJAX
+                let formData = new FormData(event.target);
+                fetch('/CreateCategory', { method: 'POST', body: formData })
+                    .then(response => response.json())
+                    .then(data => console.log(data))
+                    .catch(error => console.error('Error:', error));
             }
+        });
+    }
 
-            const newHashtag = document.createElement('li');
-            newHashtag.textContent = hashtagName;
+    document.addEventListener('keydown', (event) => {
+        currentInput.push(event.key);
+        if (currentInput.length > konamiCode.length) {
+            currentInput.shift();
+        }
 
-            document.getElementById('hashtagsList').appendChild(newHashtag);
-
-            document.querySelector('.createHashtags').value = ''; // Clear the input field
-
-            // Send form data using AJAX
-            let formData = new FormData(event.target);
-            fetch('/CreateCategory', { method: 'POST', body: formData })
-                .then(response => response.json())
-                .then(data => console.log(data));
+        if (currentInput.join('') === konamiCode.join('')) {
+            launchConfetti();
+            alert('Konami Code activated!');
         }
     });
+
+    function launchConfetti() {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+        });
+    }
 });
