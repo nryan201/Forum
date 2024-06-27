f0c73dac3f5aa2603e7a1eb9b9d83c60fa2ca650
carkevin <kevin.carles@ynov.com>
2024-06-27 16:02:17 +0200

    all merge

--- template/script/scroller.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    const mainSection = document.querySelector('.main-section');
-
-    mainSection.addEventListener('scroll', function() {
-        console.log('scrolling');
-    });
-});
