1ed7f980101a3cd4810e333af92824fecc478cf1
nryan <ryan.nedjari@ynov.com>
2024-07-04 13:46:43 +0200

    [UserAuthentification] add working Facebook

--- back/authentificationGoogle.go
+++ back/authentificationGoogle.go
@@ -52,27 +52,27 @@ func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	}
 	defer response.Body.Close()
 
-	var googleUser struct {
+	var GoogleUser struct {
 		ID      string `json:"id"`
 		Email   string `json:"email"`
 		Name    string `json:"name"`
 		Picture string `json:"picture"`
 	}
 
-	if err := json.NewDecoder(response.Body).Decode(&googleUser); err != nil {
+	if err := json.NewDecoder(response.Body).Decode(&GoogleUser); err != nil {
 		log.Printf("Could not decode user data: %s\n", err.Error())
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
 	// Nettoyer les valeurs pour éliminer les caractères invisibles
-	googleUser.ID = strings.TrimSpace(googleUser.ID)
-	googleUser.Name = strings.TrimSpace(googleUser.Name)
-	googleUser.Email = strings.TrimSpace(googleUser.Email)
+	GoogleUser.ID = strings.TrimSpace(GoogleUser.ID)
+	GoogleUser.Name = strings.TrimSpace(GoogleUser.Name)
+	GoogleUser.Email = strings.TrimSpace(GoogleUser.Email)
 
 	// Validation des données pour s'assurer qu'il n'y a pas de caractères non visibles
-	if !isValidString(googleUser.ID) || !isValidString(googleUser.Name) || !isValidString(googleUser.Email) {
-		log.Printf("Invalid data found in user details: ID: %s, Name: %s, Email: %s\n", googleUser.ID, googleUser.Name, googleUser.Email)
+	if !isValidString(GoogleUser.ID) || !isValidString(GoogleUser.Name) || !isValidString(GoogleUser.Email) {
+		log.Printf("Invalid data found in user details: ID: %s, Name: %s, Email: %s\n", GoogleUser.ID, GoogleUser.Name, GoogleUser.Email)
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
@@ -86,9 +86,9 @@ func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	log.Println("Database opened successfully")
 
 	var userGoogleID string
-	err = db.QueryRow("SELECT id FROM users WHERE id = ? OR email = ?", googleUser.ID, googleUser.Email).Scan(&userGoogleID)
+	err = db.QueryRow("SELECT id FROM users WHERE id = ? OR email = ?", GoogleUser.ID, GoogleUser.Email).Scan(&userGoogleID)
 	if errors.Is(err, sql.ErrNoRows) {
-		log.Printf("Attempting to insert new user with ID: %s, Name: %s, Email: %s", googleUser.ID, googleUser.Name, googleUser.Email)
+		log.Printf("Attempting to insert new user with ID: %s, Name: %s, Email: %s", GoogleUser.ID, GoogleUser.Name, GoogleUser.Email)
 		statement, err := db.Prepare("INSERT INTO users (id, username, email, role) VALUES (?, ?, ?, ?)")
 		if err != nil {
 			log.Fatal("Failed to prepare statement: ", err)
@@ -96,22 +96,22 @@ func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 		defer statement.Close()
 
 		// Ajout de journaux pour vérifier les types de données
-		log.Printf("Inserting user with ID (type: %T): %s", googleUser.ID, googleUser.ID)
-		log.Printf("Inserting user with Name (type: %T): %s", googleUser.Name, googleUser.Name)
-		log.Printf("Inserting user with Email (type: %T): %s", googleUser.Email, googleUser.Email)
+		log.Printf("Inserting user with ID (type: %T): %s", GoogleUser.ID, GoogleUser.ID)
+		log.Printf("Inserting user with Name (type: %T): %s", GoogleUser.Name, GoogleUser.Name)
+		log.Printf("Inserting user with Email (type: %T): %s", GoogleUser.Email, GoogleUser.Email)
 
-		_, err = statement.Exec(googleUser.ID, googleUser.Name, googleUser.Email, "user")
+		_, err = statement.Exec(GoogleUser.ID, GoogleUser.Name, GoogleUser.Email, "user")
 		if err != nil {
 			log.Printf("Failed to insert new user: %s", err)
 		} else {
 			log.Println("New user inserted successfully")
-			fmt.Fprintf(w, "Connexion réussie. Bienvenue %s! (ID utilisateur: %s)", googleUser.Name, googleUser.ID)
+			fmt.Fprintf(w, "Connexion réussie. Bienvenue %s! (ID utilisateur: %s)", GoogleUser.Name, GoogleUser.ID)
 		}
 	} else if err != nil {
 		log.Fatal("Failed to query existing user: ", err)
 	} else {
 		log.Printf("User found with ID: %s", userGoogleID)
-		fmt.Fprintf(w, "Welcome back %s! (User ID: %s)", googleUser.Name, userGoogleID)
+		fmt.Fprintf(w, "Welcome back %s! (User ID: %s)", GoogleUser.Name, userGoogleID)
 	}
 }
 
