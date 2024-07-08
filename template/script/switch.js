1ff4398023eb869446f0deba0a113188d6fb0a43
louis <louis.margras@ynov.com>
2024-07-08 18:50:43 +0200

    [main/ReviewLouis] commit afin pour checkout

--- template/script/switch.js
+++ template/script/switch.js
@@ -1,3 +1,4 @@
+//for the profil/connexion button
 document.addEventListener("DOMContentLoaded", function () {
     fetch("/api/check-auth")
         .then(function (response) {
@@ -17,6 +18,43 @@ document.addEventListener("DOMContentLoaded", function () {
                     console.log("User is not authenticated");
                     button.textContent = "Connexion";
                     button.href = "/connexion";
+                }
+            } else {
+                console.error("Auth button not found");
+            }
+        })
+        .catch(function (error) {
+            console.error("Error fetching /api/check-auth:", error);
+        });
+});
+
+//for the create post button
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/api/check-auth")
+        .then(function (response) {
+            console.log("Received response from /api/check-auth");
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("Data received from /api/check-auth:", data);
+            const authButton = document.getElementById('auth-button');
+            const createPostButton = document.getElementById('create-post-button');
+
+            if (authButton) {
+                console.log("Auth button found:", authButton);
+                if (data.authenticated) {
+                    console.log("User is authenticated");
+                    authButton.textContent = "Create";
+                    authButton.href = "/createpost";
+                    // Show create post button
+                    if (createPostButton) {
+                        createPostButton.style.display = 'inline-block';
+                    }
+                } else {
+                    // Hide create post button
+                    if (createPostButton) {
+                        createPostButton.style.display = 'none';
+                    }
                 }
             } else {
                 console.error("Auth button not found");
